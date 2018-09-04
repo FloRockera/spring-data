1864aaeea2e803a8084093eb626336c10c263f7d
Flora Calbris <flora.calbris@gmail.com>
2018-09-04 16:59:01 +0200

    data

--- src/main/java/com/formation/AppConfigMess.java
+++ src/main/java/com/formation/AppConfigMess.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -15,6 +16,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.dao.MessageDao;
+import com.formation.repository.MessageRepository;
 import com.formation.service.IMessageService;
 import com.formation.user.ChatConsole;
 import com.jdbc.MariaDBConfig;
@@ -22,6 +24,7 @@ import com.jdbc.MariaDBConfig;
 @Configuration
 @Import(MariaDBConfig.class)
 @EnableTransactionManagement
+@EnableJpaRepositories(basePackageClasses = MessageRepository.class)
 @ComponentScan(basePackageClasses = { IMessageService.class, ChatConsole.class, MessageDao.class })
 
 public class AppConfigMess {
